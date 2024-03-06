4dc60070297492e2c7c642dabd0bde2afb7af666
root <root@ip-172-31-84-24.ec2.internal>
2024-03-06 16:13:53 +0000

    change web-monitor delay

--- containers/web_server_monitor/src/web_monitor.sh
+++ containers/web_server_monitor/src/web_monitor.sh
@@ -2,7 +2,7 @@
 while :; do
 	echo; echo; date
 	echo "Testing the Simple Web Server..."
-	if nc -w 1 localhost 8000; then
+	if nc -w 10 localhost 8000; then
 		echo "The Simple Web Server is accepting connections"
 	else
 		echo "Something is wrong with the Simple Web Server"
---
+++ start.sh
@@ -0,0 +1,12 @@
+kind delete cluster
+docker rm -fv kind-regisrty
+cd /var/opt/source/k8s/kind/
+./kind_create.sh
+cd /var/opt/source/containers/simple_web_server
+sdlc/build
+cd /var/opt/source/containers/web_server_monitor/
+sdlc/build
+cd /var/opt/source/k8s/simple-web/
+kubectl apply -f simple-web.yml
+sleep 10
+kubectl port-forward service/simple-web 8000:8000 --address='0.0.0.0'
